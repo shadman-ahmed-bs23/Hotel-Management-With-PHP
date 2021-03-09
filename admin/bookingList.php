8317d3fb79fddad54c7c0ecd5b8365eb13f6b06c
Shadman Ahmed <shadmanahmed874@gmail.com>
2021-03-09 17:18:22 +0600

    Add room list in room status page

--- admin/bookingList.php
+++ admin/bookingList.php
@@ -101,17 +101,6 @@
     {
         if (isset($_POST['confirmBtn'])) {
 
-            // echo "
-            //   <script>
-            //   if (confirm('Are you sure you want to save this thing into the database?')) {
-            //     // Save it!
-            //     console.log('Thing was saved to the database.');
-            //   } else {
-            //     // Do nothing!
-            //     console.log('Thing was not saved to the database.');
-            //   }
-            //   </script>";
-
             $ids = $_POST['confirmBtn'];
 
             $str_arr = explode("+", $ids);
